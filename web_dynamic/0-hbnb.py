692e26ca98906e529076dabc7692d3e6f4607861
nonaaaaaaaaa <nehalserag44@gmail.com>
2024-05-03 07:48:11 -0700

    webdy

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
+import uuid
 from models import storage
 from models.state import State
 from models.city import City
@@ -7,6 +8,7 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.teardown_appcontext
@@ -17,7 +19,8 @@ def close_db(error):
 @app.route('/0-hbnb/')
 def updated_route():
     """ Updated route logic here """
-    return render_template('0-hbnb.html')
+ cache_id = str(uuid.uuid4())
+    return render_template('0-hbnb.html', cache_id=cache_id)
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
